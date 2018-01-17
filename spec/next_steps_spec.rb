3515e8e6ae0b27444f082112ae3d2bf67081725a
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2018-01-17 13:18:18 +0000

    Add 'account updated' notice
    After user has completed the reset password flow they are redirected to
    the page "/next-steps?account-updated".
    
    This displays a short notice at the top of the page (similar to the
    "?success" message) to indicate the flow was completed successfully

--- spec/next_steps_spec.rb
+++ spec/next_steps_spec.rb
@@ -19,4 +19,10 @@ RSpec.describe 'Next steps', :type => :feature do
     expect(page.status_code).to eq(200)
   end
 
+  it 'should be able to load page successfully with account-updated message' do
+    visit '/next-steps?account-updated'
+    expect(page).to have_selector('.account-updated'), 'Expected to see "Your account has been updated" message'
+    expect(page.status_code).to eq(200)
+  end
+
 end
